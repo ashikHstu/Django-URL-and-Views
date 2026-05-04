4f407122d99d32c0fce30cac8827f9767b015ff4
ashikHstu <ashik.cse.hstu@gmail.com>
2026-05-05 00:44:50 +0600

    dynamic logic views

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -21,6 +21,17 @@ monthly_challenges = {
     "december":"December clicked"
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items+=f"<li><a href=\"{month_path}\">{capitalized_month} </a></li>"
+    response_data =f"<ul>{list_items} </ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -34,8 +45,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request,month):
     try:
         response = monthly_challenges[month]
-        return HttpResponse(response)
+        response_data = f"<h1>{response}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
     
 
