778b42c22fe15577338edbe90d0963dcdc82cb60
ashikHstu <ashik.cse.hstu@gmail.com>
2026-05-05 00:21:23 +0600

    redirect url learning

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
 def index(request):
     return HttpResponse("This works!")
 
+monthly_challenges = {
+    "january":"January clicked",
+    "february":"February clicked",
+    "march":"March clicked",
+    "april":"April clicked",
+    "may":"May clicked",
+    "june":"June clicked",
+    "july":"July licked",
+    "august":"August clicked",
+    "september":"September clicked",
+    "october":"October clicked",
+    "november":"November clicked",
+    "december":"December clicked"
+}
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month>len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challenges/"+redirect_month)
 
 def monthly_challenge(request,month):
-    response = "Not supported."
-    if month=="January":
-        response = "Januray clicked"
-    elif month == "February":
-        response = "February clicked"
-    return HttpResponse(response)
+    try:
+        response = monthly_challenges[month]
+        return HttpResponse(response)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+    
 
